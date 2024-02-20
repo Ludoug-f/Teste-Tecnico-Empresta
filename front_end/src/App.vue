0550582dd6bed282147fb1b259cfeed6eaffa07a
Luciano Freitas <lucianoguns123@gmail.com>
2024-02-20 00:01:44 -0300

    criando o titulo

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <TheHeader/>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
   }
 }
 </script>
---
+++ front_end/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="home">Simulador de empréstimo - Bem Melhor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
+
+<style scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  } 
+</style>
